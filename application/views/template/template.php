afbee99d282a5dea6be7b1f17174d46b79efc548
uun setiawati <uunsetiawati@gmail.com>
2024-10-16 16:25:12 +0700

    ganti favicon

--- application/views/template/template.php
+++ application/views/template/template.php
@@ -9,7 +9,7 @@
     <meta content="" name="description">
 
     <!-- Favicon -->
-    <link href="<?=base_url('assets/img/favicon.png')?>" rel="icon">
+    <link href="<?=base_url('assets/img/favicon.jpg')?>" rel="icon">
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
